e7d0750cd373d145a953e45f6a7f270f7910b471
Bram Pauwelyn <brampauw@gmail.com>
2018-01-12 21:55:25 +0100

    wip test with espeak

--- modules/life.py
+++ modules/life.py
@@ -1,6 +1,6 @@
 #You can import any modules required here
-from num2words import num2words
-from subprocess import call
+import subprocess
+text = '"Hello world"'
 
 #This is name of the module - it can be anything you want
 moduleName = "life"
@@ -8,19 +8,11 @@ moduleName = "life"
 #These are the words you must say for this module to be executed
 commandWords = ["meaning","life"]
 
-cmd_beg= 'espeak '
-cmd_end= ' | aplay /home/pi/Desktop/Text.wav  2>/dev/null' # To play back the stored .wav file and to dump the std errors to /dev/null
-cmd_out= '--stdout > /home/pi/Desktop/Text.wav ' # To store the voice file
-text = ""
+
 
 #This is the main function which will be execute when the above command words are said
 def execute(command):
-    print(text)
-    #Replacing ' ' with '_' to identify words in the text entered
-    text = text.replace(' ', '_')
-
-    #Calls the Espeak TTS Engine to read aloud a Text
-    call([cmd_beg+cmd_out+text+cmd_end], shell=True)
+    subprocess.call('espeak '+text, shell=True)
     print("\n")
     print("------------------The meaning of life is 42-------------------")
     print("\n")
