062d7fc9e091ecf34ffb31a7a7f2002bef2fa4f1
Bram Pauwelyn <brampauw@gmail.com>
2018-01-13 22:15:16 +0100

    add love module

--- modules/alex.py
+++ modules/alex.py
@@ -1,18 +1,14 @@
 #You can import any modules required here
 import subprocess
-text = '"Bram aime Alex fort comme sa friteuse"'
+text = '""'
 
 #This is name of the module - it can be anything you want
-moduleName = "alex"
+moduleName = ""
 
 #These are the words you must say for this module to be executed
-commandWords = ["alex"]
-
+commandWords = [""]
 
 
 #This is the main function which will be execute when the above command words are said
 def execute(command):
     subprocess.call('espeak -s80 -vfr '+text, shell=True)
-    print("\n")
-    print("------------------Alexandra is the best-------------------")
-    print("\n")
---
+++ modules/love.py
@@ -0,0 +1,33 @@
+#You can import any required modules here
+from sense_hat import SenseHat
+sense = SenseHat()
+
+# No color
+O = (0,0,0)
+
+# Set pink color
+P = (255,100,150)
+
+# Make the pixel heart
+pixel_heart = [
+O, O, O, O, O, O, O, O,
+O, P, P, O, O, P, P, O,
+P, P, P, P, P, P, P, P,
+P, P, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O
+]
+
+#This can be anything you want
+moduleName = "love"
+
+#All of the words must be heard in order for this module to be executed
+commandWords = ["love"]
+
+def execute(command):
+    #Write anything you want to be executed when the commandWords are heard
+    #The 'command' parameter is the command you speak
+    sense.set_pixels(pixel_heart)
+    return
